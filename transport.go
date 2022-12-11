4c16d00c238fd1ce16a77c49c7baf6a95098bbbd
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2022-12-11 21:31:23 +0800

    finish transConn.decodeWithoutNAT, update decodeWithNAT, add checkSrcMAC.

--- transport.go
+++ transport.go
@@ -1,6 +1,7 @@
 package accelerator
 
 import (
+	"bytes"
 	"encoding/binary"
 	"io"
 	"net"
@@ -8,13 +9,16 @@ import (
 
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 )
 
 type transConn struct {
 	ctx *Server
 
-	conn net.Conn
-	nat  bool
+	nat    bool
+	handle *pcap.Handle
+	conn   net.Conn
+	token  sessionToken
 
 	eth   *layers.Ethernet
 	arp   *layers.ARP
@@ -28,11 +32,15 @@ type transConn struct {
 	parser  *gopacket.DecodingLayerParser
 	decoded *[]gopacket.LayerType
 
+	// check client is income new
+	srcMAC []net.HardwareAddr
+
 	slOpt gopacket.SerializeOptions
 	slBuf gopacket.SerializeBuffer
 }
 
-func (srv *Server) newTransportConn(conn net.Conn) *transConn {
+func (srv *Server) newTransportConn(conn net.Conn, token sessionToken) *transConn {
+	nat := srv.config.NAT.Enabled
 	eth := new(layers.Ethernet)
 	arp := new(layers.ARP)
 	ipv4 := new(layers.IPv4)
@@ -41,13 +49,21 @@ func (srv *Server) newTransportConn(conn net.Conn) *transConn {
 	icmp6 := new(layers.ICMPv6)
 	tcp := new(layers.TCP)
 	udp := new(layers.UDP)
-	parser := gopacket.NewDecodingLayerParser(
-		layers.LayerTypeEthernet,
-		eth, arp,
-		ipv4, icmp4,
-		ipv6, icmp6,
-		tcp, udp,
-	)
+	var parser *gopacket.DecodingLayerParser
+	if nat {
+		parser = gopacket.NewDecodingLayerParser(
+			layers.LayerTypeEthernet,
+			eth, arp,
+			ipv4, icmp4,
+			ipv6, icmp6,
+			tcp, udp,
+		)
+	} else {
+		parser = gopacket.NewDecodingLayerParser(
+			layers.LayerTypeEthernet,
+			eth,
+		)
+	}
 	parser.IgnoreUnsupported = true
 	decoded := new([]gopacket.LayerType)
 	slOpt := gopacket.SerializeOptions{
@@ -57,8 +73,10 @@ func (srv *Server) newTransportConn(conn net.Conn) *transConn {
 	slBuf := gopacket.NewSerializeBuffer()
 	tc := transConn{
 		ctx:     srv,
+		nat:     nat,
+		handle:  srv.handle,
 		conn:    conn,
-		nat:     srv.config.NAT.Enabled,
+		token:   token,
 		eth:     eth,
 		arp:     arp,
 		ipv4:    ipv4,
@@ -75,8 +93,8 @@ func (srv *Server) newTransportConn(conn net.Conn) *transConn {
 	return &tc
 }
 
-func (t *transConn) transport() {
-	_ = t.conn.SetDeadline(time.Time{})
+func (tc *transConn) transport() {
+	_ = tc.conn.SetDeadline(time.Time{})
 	var (
 		size uint16
 		err  error
@@ -84,37 +102,79 @@ func (t *transConn) transport() {
 	buf := make([]byte, maxPacketSize)
 	for {
 		// read frame packet size
-		_, err = io.ReadFull(t.conn, buf[:frameHeaderSize])
+		_, err = io.ReadFull(tc.conn, buf[:frameHeaderSize])
 		if err != nil {
 			return
 		}
 		size = binary.BigEndian.Uint16(buf[:frameHeaderSize])
 		// read frame packet
-		_, err = io.ReadFull(t.conn, buf[:size])
+		_, err = io.ReadFull(tc.conn, buf[:size])
 		if err != nil {
 			return
 		}
-		t.decode(buf[:size])
+		if tc.nat {
+			tc.decodeWithNAT(buf[:size])
+		} else {
+			tc.decodeWithoutNAT(buf[:size])
+		}
 	}
 }
 
-func (t *transConn) decode(buf []byte) {
-	err := t.parser.DecodeLayers(buf, t.decoded)
+func (tc *transConn) decodeWithoutNAT(buf []byte) {
+	err := tc.parser.DecodeLayers(buf, tc.decoded)
 	if err != nil {
 		return
 	}
+	decoded := *tc.decoded
+	if len(decoded) < 1 {
+		return
+	}
+	if decoded[0] != layers.LayerTypeEthernet {
+		return
+	}
+	tc.checkSrcMAC()
+	_ = tc.handle.WritePacketData(buf)
+}
 
-	for i := 0; i < len(); i++ {
-		if decoded[i] != layers.LayerTypeEthernet {
-			continue
+func (tc *transConn) decodeWithNAT(buf []byte) {
+	err := tc.parser.DecodeLayers(buf, tc.decoded)
+	if err != nil {
+		return
+	}
+	decoded := *tc.decoded
+	for i := 0; i < len(decoded); i++ {
+		switch decoded[i] {
+		case layers.LayerTypeEthernet:
+		case layers.LayerTypeARP:
+		case layers.LayerTypeIPv4:
+		case layers.LayerTypeIPv6:
+		case layers.LayerTypeICMPv4:
+		case layers.LayerTypeICMPv6:
+		case layers.LayerTypeTCP:
+		case layers.LayerTypeUDP:
 		}
 	}
 
-	if !srv.useNAT {
-		err = srv.handle.WritePacketData(buf[:size])
-		if err != nil {
-			return
+	// err = tc.handle.WritePacketData()
+	// if err != nil {
+	// 	return
+	// }
+}
+
+func (tc *transConn) checkSrcMAC() {
+	var exist bool
+	for i := 0; i < len(tc.srcMAC); i++ {
+		if bytes.Equal(tc.srcMAC[i], tc.eth.SrcMAC) {
+			exist = true
+			break
 		}
-		continue
 	}
+	if exist {
+		return
+	}
+	// must copy, because DecodeLayers use reference
+	mac := [6]byte{}
+	copy(mac[:], tc.eth.SrcMAC)
+	tc.srcMAC = append(tc.srcMAC, mac[:])
+	tc.ctx.bindMAC(tc.token, mac)
 }
