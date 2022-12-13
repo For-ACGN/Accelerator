dad194a1902cfcc68bbbb428476ef13b7c7dcd13
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2022-12-13 20:01:01 +0800

    add GeneratePasswordHash for generate password hash, it use pbkdf2 algorithm.

--- hash.go
+++ hash.go
@@ -5,7 +5,14 @@ import (
 	"encoding/hex"
 
 	"github.com/pkg/errors"
+	"golang.org/x/crypto/pbkdf2"
 )
+
+// GeneratePasswordHash is used to generate password hash.
+func GeneratePasswordHash(password []byte) string {
+	key := pbkdf2.Key(password, []byte("acc"), 8192, sha256.Size, sha256.New)
+	return hex.EncodeToString(key)
+}
 
 func decodePasswordHash(hash string) ([]byte, error) {
 	passHash, err := hex.DecodeString(hash)
