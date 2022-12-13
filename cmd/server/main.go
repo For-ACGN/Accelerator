a30c112f037fa2de5bc37334151216b021553d72
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2022-12-13 20:13:27 +0800

    finish main package about accelerator server, it use go-toml for parse config file.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,48 @@
 package main
 
-func init() {
+import (
+	"bytes"
+	"flag"
+	"log"
+	"os"
+	"os/signal"
 
+	"github.com/pelletier/go-toml/v2"
+
+	"github.com/For-ACGN/Accelerator"
+)
+
+var cfgPath string
+
+func init() {
+	flag.StringVar(&cfgPath, "c", "config.toml", "configuration file path")
+	flag.Parse()
 }
 
 func main() {
+	cfgData, err := os.ReadFile(cfgPath)
+	checkError(err)
+	decoder := toml.NewDecoder(bytes.NewReader(cfgData))
+	decoder.DisallowUnknownFields()
+	var config accelerator.ServerConfig
+	err = decoder.Decode(&config)
+	checkError(err)
 
+	server, err := accelerator.NewServer(&config)
+	checkError(err)
+	server.Run()
+
+	// stop signal
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt)
+	<-signalCh
+
+	err = server.Close()
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
