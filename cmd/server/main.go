f5b1f1633d6bdf8ceacddc9a314361ca1506385f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2022-12-17 15:09:34 +0800

    server/main.go: add listDevices for list network device in current system.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/google/gopacket/pcap"
 	"github.com/pelletier/go-toml/v2"
 
 	"github.com/For-ACGN/Accelerator"
@@ -16,11 +17,13 @@ import (
 var (
 	cfgPath  string
 	password string
+	listDev  bool
 )
 
 func init() {
 	flag.StringVar(&cfgPath, "config", "config.toml", "configuration file path")
 	flag.StringVar(&password, "gen-hash", "", "generate password hash")
+	flag.BoolVar(&listDev, "list-dev", false, "list network interface")
 	flag.Parse()
 }
 
@@ -28,6 +31,10 @@ func main() {
 	if password != "" {
 		hash := accelerator.GeneratePasswordHash([]byte(password))
 		fmt.Println("password hash:", hash)
+		return
+	}
+	if listDev {
+		listDevices()
 		return
 	}
 
@@ -51,6 +58,44 @@ func main() {
 
 	err = server.Close()
 	checkError(err)
+}
+
+func listDevices() {
+	devs, err := pcap.FindAllDevs()
+	checkError(err)
+	type item struct {
+		name string
+		ip   string
+	}
+	var (
+		maxNameLen int
+		items      []*item
+	)
+	buf := bytes.NewBuffer(make([]byte, 0, 64))
+	for i := 0; i < len(devs); i++ {
+		dev := devs[i]
+		l := len(dev.Addresses)
+		for j := 0; j < l; j++ {
+			buf.WriteString(dev.Addresses[j].IP.String())
+			if j != l-1 {
+				buf.WriteString(", ")
+			}
+		}
+		l = len(dev.Name)
+		if l > maxNameLen {
+			maxNameLen = l
+		}
+		items = append(items, &item{
+			name: dev.Name,
+			ip:   buf.String(),
+		})
+		buf.Reset()
+	}
+	format := fmt.Sprintf("%%-%ds  %%s\n", maxNameLen)
+	fmt.Printf(format, "[Device Name]", "[IP Address]")
+	for i := 0; i < len(items); i++ {
+		fmt.Printf(format, items[i].name, items[i].ip)
+	}
 }
 
 func checkError(err error) {
