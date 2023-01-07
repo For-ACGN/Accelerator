24116208440a93219ead5ba4608ad3e018ad41e1
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2023-01-07 20:01:30 +0800

    add simple udp client and server to test tool for test accelerator client and server.

---
+++ testtool/udpclient/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+)
+
+var (
+	localAddr  string
+	remoteAddr string
+)
+
+func init() {
+	flag.StringVar(&localAddr, "l", "0.0.0.0:0", "local address")
+	flag.StringVar(&remoteAddr, "r", "127.0.0.1:3080", "server address")
+	flag.Parse()
+}
+
+func main() {
+	lAddr, err := net.ResolveUDPAddr("udp", localAddr)
+	checkError(err)
+	rAddr, err := net.ResolveUDPAddr("udp", remoteAddr)
+	checkError(err)
+	pc, err := net.ListenUDP("udp", lAddr)
+	checkError(err)
+	buf := make([]byte, 64)
+	_, _ = pc.WriteTo(buf, rAddr)
+	for {
+		_, addr, err := pc.ReadFrom(buf)
+		checkError(err)
+		fmt.Println(addr)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ testtool/udpserver/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+var address string
+
+func init() {
+	flag.StringVar(&address, "addr", "0.0.0.0:3080", "server address")
+	flag.Parse()
+}
+
+func main() {
+	pc, err := net.ListenPacket("udp", address)
+	checkError(err)
+	buf := make([]byte, 64)
+	for {
+		_, addr, err := pc.ReadFrom(buf)
+		checkError(err)
+		go func(addr net.Addr) {
+			fmt.Println(addr)
+			for i := 0; i < 60*240; i++ {
+				_, _ = pc.WriteTo(buf, addr)
+				time.Sleep(time.Second / 60)
+			}
+		}(addr)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
