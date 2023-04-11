30546784ed2f0fdb202e07afaf7d776de740f346
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2023-04-11 16:54:03 +0800

    add testMustDecodeHex and frame list, update TestCFHWriter_searchDictionary.

--- compress_test.go
+++ compress_test.go
@@ -2,6 +2,7 @@ package accelerator
 
 import (
 	"bytes"
+	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -29,42 +30,88 @@ var (
 	testUDPH1 = "fb7b003500385f66"
 	testUDPH2 = "fb7b003600385f66"
 
-	testIPv4TCPFrame1 = []byte(testEthernet1 + testIPv4H1 + testTCPH1)
-	testIPv4TCPFrame2 = []byte(testEthernet1 + testIPv4H1 + testTCPH2)
-	testIPv4TCPFrame3 = []byte(testEthernet1 + testIPv4H2 + testTCPH1)
-	testIPv4TCPFrame4 = []byte(testEthernet1 + testIPv4H2 + testTCPH2)
-	testIPv4TCPFrame5 = []byte(testEthernet2 + testIPv4H1 + testTCPH1)
-	testIPv4TCPFrame6 = []byte(testEthernet2 + testIPv4H1 + testTCPH2)
-	testIPv4TCPFrame7 = []byte(testEthernet2 + testIPv4H2 + testTCPH1)
-	testIPv4TCPFrame8 = []byte(testEthernet2 + testIPv4H2 + testTCPH2)
+	testIPv4TCPFrame1 = testMustDecodeHex(testEthernet1 + testIPv4H1 + testTCPH1)
+	testIPv4TCPFrame2 = testMustDecodeHex(testEthernet1 + testIPv4H1 + testTCPH2)
+	testIPv4TCPFrame3 = testMustDecodeHex(testEthernet1 + testIPv4H2 + testTCPH1)
+	testIPv4TCPFrame4 = testMustDecodeHex(testEthernet1 + testIPv4H2 + testTCPH2)
+	testIPv4TCPFrame5 = testMustDecodeHex(testEthernet2 + testIPv4H1 + testTCPH1)
+	testIPv4TCPFrame6 = testMustDecodeHex(testEthernet2 + testIPv4H1 + testTCPH2)
+	testIPv4TCPFrame7 = testMustDecodeHex(testEthernet2 + testIPv4H2 + testTCPH1)
+	testIPv4TCPFrame8 = testMustDecodeHex(testEthernet2 + testIPv4H2 + testTCPH2)
 
-	testIPv4UDPFrame1 = []byte(testEthernet1 + testIPv4H3 + testUDPH1)
-	testIPv4UDPFrame2 = []byte(testEthernet1 + testIPv4H3 + testUDPH2)
-	testIPv4UDPFrame3 = []byte(testEthernet1 + testIPv4H4 + testUDPH1)
-	testIPv4UDPFrame4 = []byte(testEthernet1 + testIPv4H4 + testUDPH2)
-	testIPv4UDPFrame5 = []byte(testEthernet2 + testIPv4H3 + testUDPH1)
-	testIPv4UDPFrame6 = []byte(testEthernet2 + testIPv4H3 + testUDPH2)
-	testIPv4UDPFrame7 = []byte(testEthernet2 + testIPv4H4 + testUDPH1)
-	testIPv4UDPFrame8 = []byte(testEthernet2 + testIPv4H4 + testUDPH2)
+	testIPv4UDPFrame1 = testMustDecodeHex(testEthernet1 + testIPv4H3 + testUDPH1)
+	testIPv4UDPFrame2 = testMustDecodeHex(testEthernet1 + testIPv4H3 + testUDPH2)
+	testIPv4UDPFrame3 = testMustDecodeHex(testEthernet1 + testIPv4H4 + testUDPH1)
+	testIPv4UDPFrame4 = testMustDecodeHex(testEthernet1 + testIPv4H4 + testUDPH2)
+	testIPv4UDPFrame5 = testMustDecodeHex(testEthernet2 + testIPv4H3 + testUDPH1)
+	testIPv4UDPFrame6 = testMustDecodeHex(testEthernet2 + testIPv4H3 + testUDPH2)
+	testIPv4UDPFrame7 = testMustDecodeHex(testEthernet2 + testIPv4H4 + testUDPH1)
+	testIPv4UDPFrame8 = testMustDecodeHex(testEthernet2 + testIPv4H4 + testUDPH2)
 
-	testIPv6TCPFrame1 = []byte(testEthernet3 + testIPv6H1 + testTCPH1)
-	testIPv6TCPFrame2 = []byte(testEthernet3 + testIPv6H1 + testTCPH2)
-	testIPv6TCPFrame3 = []byte(testEthernet3 + testIPv6H2 + testTCPH1)
-	testIPv6TCPFrame4 = []byte(testEthernet3 + testIPv6H2 + testTCPH2)
-	testIPv6TCPFrame5 = []byte(testEthernet4 + testIPv6H1 + testTCPH1)
-	testIPv6TCPFrame6 = []byte(testEthernet4 + testIPv6H1 + testTCPH2)
-	testIPv6TCPFrame7 = []byte(testEthernet4 + testIPv6H2 + testTCPH1)
-	testIPv6TCPFrame8 = []byte(testEthernet4 + testIPv6H2 + testTCPH2)
+	testIPv6TCPFrame1 = testMustDecodeHex(testEthernet3 + testIPv6H1 + testTCPH1)
+	testIPv6TCPFrame2 = testMustDecodeHex(testEthernet3 + testIPv6H1 + testTCPH2)
+	testIPv6TCPFrame3 = testMustDecodeHex(testEthernet3 + testIPv6H2 + testTCPH1)
+	testIPv6TCPFrame4 = testMustDecodeHex(testEthernet3 + testIPv6H2 + testTCPH2)
+	testIPv6TCPFrame5 = testMustDecodeHex(testEthernet4 + testIPv6H1 + testTCPH1)
+	testIPv6TCPFrame6 = testMustDecodeHex(testEthernet4 + testIPv6H1 + testTCPH2)
+	testIPv6TCPFrame7 = testMustDecodeHex(testEthernet4 + testIPv6H2 + testTCPH1)
+	testIPv6TCPFrame8 = testMustDecodeHex(testEthernet4 + testIPv6H2 + testTCPH2)
 
-	testIPv6UDPFrame1 = []byte(testEthernet3 + testIPv6H3 + testUDPH1)
-	testIPv6UDPFrame2 = []byte(testEthernet3 + testIPv6H3 + testUDPH2)
-	testIPv6UDPFrame3 = []byte(testEthernet3 + testIPv6H4 + testUDPH1)
-	testIPv6UDPFrame4 = []byte(testEthernet3 + testIPv6H4 + testUDPH2)
-	testIPv6UDPFrame5 = []byte(testEthernet4 + testIPv6H3 + testUDPH1)
-	testIPv6UDPFrame6 = []byte(testEthernet4 + testIPv6H3 + testUDPH2)
-	testIPv6UDPFrame7 = []byte(testEthernet4 + testIPv6H4 + testUDPH1)
-	testIPv6UDPFrame8 = []byte(testEthernet4 + testIPv6H4 + testUDPH2)
+	testIPv6UDPFrame1 = testMustDecodeHex(testEthernet3 + testIPv6H3 + testUDPH1)
+	testIPv6UDPFrame2 = testMustDecodeHex(testEthernet3 + testIPv6H3 + testUDPH2)
+	testIPv6UDPFrame3 = testMustDecodeHex(testEthernet3 + testIPv6H4 + testUDPH1)
+	testIPv6UDPFrame4 = testMustDecodeHex(testEthernet3 + testIPv6H4 + testUDPH2)
+	testIPv6UDPFrame5 = testMustDecodeHex(testEthernet4 + testIPv6H3 + testUDPH1)
+	testIPv6UDPFrame6 = testMustDecodeHex(testEthernet4 + testIPv6H3 + testUDPH2)
+	testIPv6UDPFrame7 = testMustDecodeHex(testEthernet4 + testIPv6H4 + testUDPH1)
+	testIPv6UDPFrame8 = testMustDecodeHex(testEthernet4 + testIPv6H4 + testUDPH2)
 )
+
+var testFrames = [][]byte{
+	testIPv4TCPFrame1,
+	testIPv4TCPFrame2,
+	testIPv4TCPFrame3,
+	testIPv4TCPFrame4,
+	testIPv4TCPFrame5,
+	testIPv4TCPFrame6,
+	testIPv4TCPFrame7,
+	testIPv4TCPFrame8,
+
+	testIPv4UDPFrame1,
+	testIPv4UDPFrame2,
+	testIPv4UDPFrame3,
+	testIPv4UDPFrame4,
+	testIPv4UDPFrame5,
+	testIPv4UDPFrame6,
+	testIPv4UDPFrame7,
+	testIPv4UDPFrame8,
+
+	testIPv6TCPFrame1,
+	testIPv6TCPFrame2,
+	testIPv6TCPFrame3,
+	testIPv6TCPFrame4,
+	testIPv6TCPFrame5,
+	testIPv6TCPFrame6,
+	testIPv6TCPFrame7,
+	testIPv6TCPFrame8,
+
+	testIPv6UDPFrame1,
+	testIPv6UDPFrame2,
+	testIPv6UDPFrame3,
+	testIPv6UDPFrame4,
+	testIPv6UDPFrame5,
+	testIPv6UDPFrame6,
+	testIPv6UDPFrame7,
+	testIPv6UDPFrame8,
+}
+
+func testMustDecodeHex(s string) []byte {
+	data, err := hex.DecodeString(s)
+	if err != nil {
+		panic(err)
+	}
+	return data
+}
 
 func TestCFHWriter(t *testing.T) {
 	t.Run("common", func(t *testing.T) {
@@ -72,31 +119,28 @@ func TestCFHWriter(t *testing.T) {
 	})
 }
 
-func TestCFHWriter_fastSearchDictionary(t *testing.T) {
-	t.Run("IPv4 TCP", func(t *testing.T) {
-		output := bytes.NewBuffer(make([]byte, 0, 1024))
+func TestCFHWriter_searchDictionary(t *testing.T) {
+	t.Run("fast", func(t *testing.T) {
+		output := bytes.NewBuffer(make([]byte, 0, 4096))
 
 		w := newCFHWriter(output)
+		for _, f := range testFrames {
+			n, err := w.Write(f)
+			require.NoError(t, err)
+			require.Equal(t, len(f), n)
+		}
 
-		n, err := w.Write(testIPv4TCPFrame1)
-		require.NoError(t, err)
-		require.Equal(t, len(testIPv4TCPFrame1), n)
-
+		r := newCFHReader(output)
+		for _, f := range testFrames {
+			buf := make([]byte, len(f))
+			n, err := r.Read(buf)
+			require.NoError(t, err)
+			require.Equal(t, len(f), n)
+			require.Equal(t, f, buf)
+		}
 	})
 
-	t.Run("IPv4 UDP", func(t *testing.T) {
+	t.Run("slow", func(t *testing.T) {
 
 	})
-
-	t.Run("IPv6 TCP", func(t *testing.T) {
-
-	})
-
-	t.Run("IPv6 UDP", func(t *testing.T) {
-
-	})
-}
-
-func TestCFHWriter_slowSearchDictionary(t *testing.T) {
-
 }
