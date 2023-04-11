df83c1577d467a140ad97d50160d9de0f10c6cc1
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2023-04-11 16:33:48 +0800

    add test data about frame for IPv4/IPv6 TCP/UDP, add TestCFHWriter.

--- compress_test.go
+++ compress_test.go
@@ -11,6 +11,71 @@ import (
 )
 
 var (
+	testEthernet1 = "d8ba1192c572d8af159ac5d10800" // IPv4
+	testEthernet2 = "d8ba1192c572d8af159ac5d20800" // IPv4
+	testEthernet3 = "d8ba1192c572d8af159ac5d386dd" // IPv6
+	testEthernet4 = "d8ba1192c572d8af159ac5d486dd" // IPv6
+
+	testIPv4H1 = "450405c8574d40003706b63514983c5fc0a81f0a" // TCP
+	testIPv4H2 = "450405c8574e40003706b63514983c5fc0a81f0a" // TCP
+	testIPv4H3 = "450405c8575d40003711b63514983c5fc0a81f0a" // UDP
+	testIPv4H4 = "450405c8575e40003711b63514983c5fc0a81f0a" // UDP
+
+	testIPv6H1 = "6043670805a0062b24108c016c2a103d000000afb00239ab24108a2aa084b4a02127e9cada1240f1" // TCP
+	testIPv6H2 = "6043670805a0062b24108c016c2a103d000000afb00239ab24108a2aa084b4a02127e9cada1240f2" // TCP
+	testIPv6H3 = "6043670805a0112b24108c016c2a103d000000afb00239ab24108a2aa084b4a02127e9cada1240f3" // UDP
+	testIPv6H4 = "6043670805a0112b24108c016c2a103d000000afb00239ab24108a2aa084b4a02127e9cada1240f4" // UDP
+
+	testTCPH1 = "01bbebd71561ddfc151e1385501003d037390000"
+	testTCPH2 = "01bbebd71661ddfc151e1385501003d037390000"
+
+	testUDPH1 = "fb7b003500385f66"
+	testUDPH2 = "fb7b003600385f66"
+
+	testIPv4TCPFrame10 = testEthernet1 + testIPv4H1 + testTCPH1
+	testIPv4TCPFrame11 = testEthernet1 + testIPv4H1 + testTCPH2
+	testIPv4TCPFrame12 = testEthernet1 + testIPv4H2 + testTCPH1
+	testIPv4TCPFrame13 = testEthernet1 + testIPv4H2 + testTCPH2
+	testIPv4TCPFrame14 = testEthernet2 + testIPv4H1 + testTCPH1
+	testIPv4TCPFrame15 = testEthernet2 + testIPv4H1 + testTCPH2
+	testIPv4TCPFrame16 = testEthernet2 + testIPv4H2 + testTCPH1
+	testIPv4TCPFrame17 = testEthernet2 + testIPv4H2 + testTCPH2
+
+	testIPv4UDPFrame20 = testEthernet1 + testIPv4H3 + testUDPH1
+	testIPv4UDPFrame21 = testEthernet1 + testIPv4H3 + testUDPH2
+	testIPv4UDPFrame22 = testEthernet1 + testIPv4H4 + testUDPH1
+	testIPv4UDPFrame23 = testEthernet1 + testIPv4H4 + testUDPH2
+	testIPv4UDPFrame24 = testEthernet2 + testIPv4H3 + testUDPH1
+	testIPv4UDPFrame25 = testEthernet2 + testIPv4H3 + testUDPH2
+	testIPv4UDPFrame26 = testEthernet2 + testIPv4H4 + testUDPH1
+	testIPv4UDPFrame27 = testEthernet2 + testIPv4H4 + testUDPH2
+
+	testIPv6TCPFrame10 = testEthernet3 + testIPv6H1 + testTCPH1
+	testIPv6TCPFrame11 = testEthernet3 + testIPv6H1 + testTCPH2
+	testIPv6TCPFrame12 = testEthernet3 + testIPv6H2 + testTCPH1
+	testIPv6TCPFrame13 = testEthernet3 + testIPv6H2 + testTCPH2
+	testIPv6TCPFrame14 = testEthernet4 + testIPv6H1 + testTCPH1
+	testIPv6TCPFrame15 = testEthernet4 + testIPv6H1 + testTCPH2
+	testIPv6TCPFrame16 = testEthernet4 + testIPv6H2 + testTCPH1
+	testIPv6TCPFrame17 = testEthernet4 + testIPv6H2 + testTCPH2
+
+	testIPv6UDPFrame20 = testEthernet3 + testIPv6H3 + testUDPH1
+	testIPv6UDPFrame21 = testEthernet3 + testIPv6H3 + testUDPH2
+	testIPv6UDPFrame22 = testEthernet3 + testIPv6H4 + testUDPH1
+	testIPv6UDPFrame23 = testEthernet3 + testIPv6H4 + testUDPH2
+	testIPv6UDPFrame24 = testEthernet4 + testIPv6H3 + testUDPH1
+	testIPv6UDPFrame25 = testEthernet4 + testIPv6H3 + testUDPH2
+	testIPv6UDPFrame26 = testEthernet4 + testIPv6H4 + testUDPH1
+	testIPv6UDPFrame27 = testEthernet4 + testIPv6H4 + testUDPH2
+)
+
+func TestCfhWriter(t *testing.T) {
+	t.Run("common", func(t *testing.T) {
+
+	})
+}
+
+var (
 	// IPv4 + TCP   54 bytes
 	frame1 = "0a5114c5bdc500ff80891a0f0800450005dceac3400080061a39ac1f14011d732b8af3e257b9b9ae99e33dd56009501003fdac0b0000"
 	frame2 = "0a5114c5bdc500ff80891a0f0800450005dceac4400080061a38ac1f14011d732b8af3e257b9b9ae9f973dd56009501003fd99d60000"
